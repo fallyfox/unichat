f3d41a4b1bdbe91521d946eb61287e17de7e8e46
Joseph Ogbu <fallyfox@gmail.com>
2025-12-11 17:52:28 +0100

    protected routes configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Brand font: https://www.1001freefonts.com/claremonts.font
+https://docs.expo.dev/router/advanced/authentication/
+https://docs.expo.dev/router/advanced/authentication-rewrites/
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,39 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const auth = true;
+  const session = false;
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {auth 
-      ?
-      <Stack.Screen
-      name="(tabs)"
-      options={{ 
-        title: "Home",
-        headerShown: false,
-      }}/>
-      :
-      <Stack.Screen
-      name="index"
-      options={{ 
-        title: "Create account",
-        headerShown: false
-      }}/>}
+      <Stack.Protected guard={session}>
+        <Stack.Screen
+        name="(tabs)"
+        options={{ 
+          title: "Home",
+          headerShown: false,
+        }}/>
+        <Stack.Screen
+        name="create-discussion"
+        options={{ 
+          title: "Create discussion",
+          headerShown: true
+        }}/>
+      </Stack.Protected>
 
-      <Stack.Screen
-      name="sign-in"
-      options={{ 
-        title: "Sign in",
-        headerShown: false
-      }}/>
-      
-      <Stack.Screen
-      name="create-discussion"
-      options={{ 
-        title: "Create discussion",
-        headerShown: true
-      }}/>
+      <Stack.Protected guard={!session}>
+        <Stack.Screen
+        name="index"
+        options={{ 
+          title: "Create account",
+          headerShown: false
+        }}/>
+        <Stack.Screen
+        name="sign-in"
+        options={{ 
+          title: "Sign in",
+          headerShown: false
+        }}/>
+      </Stack.Protected>
     </Stack>
   )
 }
